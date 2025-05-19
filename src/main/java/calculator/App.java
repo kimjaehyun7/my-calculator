d10baf81d33666fa886a3f985a76b08f63c25ca9
jaehyun <wogusrla77@naver.com>
2025-05-19 22:01:21 +0900

    feat: CalculatorController 클래스 생성 및 실행 메서드 run() 정의

---
+++ src/main/java/calculator/App.java
@@ -0,0 +1,9 @@
+package calculator;
+
+import calculator.controller.CalculatorController;
+
+public class App {
+    public static void main(String[] args) {
+        new CalculatorController().run();
+    }
+}
